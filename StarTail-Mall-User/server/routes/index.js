435978a5e8fb60e380208a065018d71a882750fe
Dolores-not-ai <542349654@qq.com>
2026-01-29 15:34:49 +0800

    为推荐和珠宝以外的tab页配置虚假后端数据

--- StarTail-Mall-User/server/routes/index.js
+++ StarTail-Mall-User/server/routes/index.js
@@ -150,15 +150,641 @@ router.get('/api/index_list/animation', function(req, res, next) {
 	res.send({
 		"code": 0,
 		"data": {
-			bigImgUrl: ''
+			bannerImgUrl: '/static/index/banner/首饰珠宝.png',
+			iconList: [{
+					id: 0,
+					title: '黄金',
+					imgUrl: '/static/index/icon/首饰珠宝/1.png'
+				},
+				{
+					id: 1,
+					title: '吊坠',
+					imgUrl: '/static/index/icon/首饰珠宝/2.png'
+				},
+				{
+					id: 2,
+					title: '耳饰',
+					imgUrl: '/static/index/icon/首饰珠宝/3.png'
+				},
+				{
+					id: 3,
+					title: '手链',
+					imgUrl: '/static/index/icon/首饰珠宝/4.png'
+				},
+				{
+					id: 4,
+					title: '戒指',
+					imgUrl: '/static/index/icon/首饰珠宝/5.png'
+				},
+				{
+					id: 5,
+					title: '项链',
+					imgUrl: '/static/index/icon/首饰珠宝/6.png'
+				},
+				{
+					id: 6,
+					title: '手镯',
+					imgUrl: '/static/index/icon/首饰珠宝/7.png'
+				},
+				{
+					id: 7,
+					title: '转运珠',
+					imgUrl: '/static/index/icon/首饰珠宝/8.png'
+				}
+			],
+			hotList: [{
+					id: 1,
+					imgUrl: '/static/index/hot/首饰珠宝/hot1.png',
+					name: '周六福字母转运珠黄金定价足金情侣款diy手链手串手绳送新年礼物',
+					nprice: '611',
+					oprice: '655',
+					discount: '9.6'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/index/hot/首饰珠宝/hot2.png',
+					name: '周六福S925银珍珠耳饰女流光温柔法式优雅送女友新年礼物',
+					nprice: '198',
+					oprice: '218',
+					discount: '9.6'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/index/hot/首饰珠宝/hot3.png',
+					name: '周六福半山半水玉髓手镯浅白绿宽条新中式国风新年礼物送女友',
+					nprice: '409',
+					oprice: '800',
+					discount: '5'
+				}
+			],
+			shopData: {
+				shopImgUrl: '/static/index/shop/1/big.png',
+				shopList: [{
+						id: 0,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 1,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 2,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					},
+					{
+						id: 3,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 4,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 5,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					}
+
+				]
+			},
+			recommendList: [{
+					id: 1,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 4,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				}
+			]
+		}
+	})
+})
+router.get('/api/index_list/game', function(req, res, next) {
+	res.send({
+		"code": 0,
+		"data": {
+			bannerImgUrl: '/static/index/banner/首饰珠宝.png',
+			iconList: [{
+					id: 0,
+					title: '黄金',
+					imgUrl: '/static/index/icon/首饰珠宝/1.png'
+				},
+				{
+					id: 1,
+					title: '吊坠',
+					imgUrl: '/static/index/icon/首饰珠宝/2.png'
+				},
+				{
+					id: 2,
+					title: '耳饰',
+					imgUrl: '/static/index/icon/首饰珠宝/3.png'
+				},
+				{
+					id: 3,
+					title: '手链',
+					imgUrl: '/static/index/icon/首饰珠宝/4.png'
+				},
+				{
+					id: 4,
+					title: '戒指',
+					imgUrl: '/static/index/icon/首饰珠宝/5.png'
+				},
+				{
+					id: 5,
+					title: '项链',
+					imgUrl: '/static/index/icon/首饰珠宝/6.png'
+				},
+				{
+					id: 6,
+					title: '手镯',
+					imgUrl: '/static/index/icon/首饰珠宝/7.png'
+				},
+				{
+					id: 7,
+					title: '转运珠',
+					imgUrl: '/static/index/icon/首饰珠宝/8.png'
+				}
+			],
+			hotList: [{
+					id: 1,
+					imgUrl: '/static/index/hot/首饰珠宝/hot1.png',
+					name: '周六福字母转运珠黄金定价足金情侣款diy手链手串手绳送新年礼物',
+					nprice: '611',
+					oprice: '655',
+					discount: '9.6'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/index/hot/首饰珠宝/hot2.png',
+					name: '周六福S925银珍珠耳饰女流光温柔法式优雅送女友新年礼物',
+					nprice: '198',
+					oprice: '218',
+					discount: '9.6'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/index/hot/首饰珠宝/hot3.png',
+					name: '周六福半山半水玉髓手镯浅白绿宽条新中式国风新年礼物送女友',
+					nprice: '409',
+					oprice: '800',
+					discount: '5'
+				}
+			],
+			shopData: {
+				shopImgUrl: '/static/index/shop/1/big.png',
+				shopList: [{
+						id: 0,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 1,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 2,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					},
+					{
+						id: 3,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 4,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 5,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					}
+
+				]
+			},
+			recommendList: [{
+					id: 1,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 4,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				}
+			]
 		}
 	})
 })
 router.get('/api/index_list/Art', function(req, res, next) {
-	res.send({})
+	res.send({
+		"code": 0,
+		"data": {
+			bannerImgUrl: '/static/index/banner/首饰珠宝.png',
+			iconList: [{
+					id: 0,
+					title: '黄金',
+					imgUrl: '/static/index/icon/首饰珠宝/1.png'
+				},
+				{
+					id: 1,
+					title: '吊坠',
+					imgUrl: '/static/index/icon/首饰珠宝/2.png'
+				},
+				{
+					id: 2,
+					title: '耳饰',
+					imgUrl: '/static/index/icon/首饰珠宝/3.png'
+				},
+				{
+					id: 3,
+					title: '手链',
+					imgUrl: '/static/index/icon/首饰珠宝/4.png'
+				},
+				{
+					id: 4,
+					title: '戒指',
+					imgUrl: '/static/index/icon/首饰珠宝/5.png'
+				},
+				{
+					id: 5,
+					title: '项链',
+					imgUrl: '/static/index/icon/首饰珠宝/6.png'
+				},
+				{
+					id: 6,
+					title: '手镯',
+					imgUrl: '/static/index/icon/首饰珠宝/7.png'
+				},
+				{
+					id: 7,
+					title: '转运珠',
+					imgUrl: '/static/index/icon/首饰珠宝/8.png'
+				}
+			],
+			hotList: [{
+					id: 1,
+					imgUrl: '/static/index/hot/首饰珠宝/hot1.png',
+					name: '周六福字母转运珠黄金定价足金情侣款diy手链手串手绳送新年礼物',
+					nprice: '611',
+					oprice: '655',
+					discount: '9.6'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/index/hot/首饰珠宝/hot2.png',
+					name: '周六福S925银珍珠耳饰女流光温柔法式优雅送女友新年礼物',
+					nprice: '198',
+					oprice: '218',
+					discount: '9.6'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/index/hot/首饰珠宝/hot3.png',
+					name: '周六福半山半水玉髓手镯浅白绿宽条新中式国风新年礼物送女友',
+					nprice: '409',
+					oprice: '800',
+					discount: '5'
+				}
+			],
+			shopData: {
+				shopImgUrl: '/static/index/shop/1/big.png',
+				shopList: [{
+						id: 0,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 1,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 2,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					},
+					{
+						id: 3,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 4,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 5,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					}
+
+				]
+			},
+			recommendList: [{
+					id: 1,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 4,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				}
+			]
+		}
+	})
 })
 router.get('/api/index_list/pet', function(req, res, next) {
-	res.send({})
+	res.send({
+		"code": 0,
+		"data": {
+			bannerImgUrl: '/static/index/banner/首饰珠宝.png',
+			iconList: [{
+					id: 0,
+					title: '黄金',
+					imgUrl: '/static/index/icon/首饰珠宝/1.png'
+				},
+				{
+					id: 1,
+					title: '吊坠',
+					imgUrl: '/static/index/icon/首饰珠宝/2.png'
+				},
+				{
+					id: 2,
+					title: '耳饰',
+					imgUrl: '/static/index/icon/首饰珠宝/3.png'
+				},
+				{
+					id: 3,
+					title: '手链',
+					imgUrl: '/static/index/icon/首饰珠宝/4.png'
+				},
+				{
+					id: 4,
+					title: '戒指',
+					imgUrl: '/static/index/icon/首饰珠宝/5.png'
+				},
+				{
+					id: 5,
+					title: '项链',
+					imgUrl: '/static/index/icon/首饰珠宝/6.png'
+				},
+				{
+					id: 6,
+					title: '手镯',
+					imgUrl: '/static/index/icon/首饰珠宝/7.png'
+				},
+				{
+					id: 7,
+					title: '转运珠',
+					imgUrl: '/static/index/icon/首饰珠宝/8.png'
+				}
+			],
+			hotList: [{
+					id: 1,
+					imgUrl: '/static/index/hot/首饰珠宝/hot1.png',
+					name: '周六福字母转运珠黄金定价足金情侣款diy手链手串手绳送新年礼物',
+					nprice: '611',
+					oprice: '655',
+					discount: '9.6'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/index/hot/首饰珠宝/hot2.png',
+					name: '周六福S925银珍珠耳饰女流光温柔法式优雅送女友新年礼物',
+					nprice: '198',
+					oprice: '218',
+					discount: '9.6'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/index/hot/首饰珠宝/hot3.png',
+					name: '周六福半山半水玉髓手镯浅白绿宽条新中式国风新年礼物送女友',
+					nprice: '409',
+					oprice: '800',
+					discount: '5'
+				}
+			],
+			shopData: {
+				shopImgUrl: '/static/index/shop/1/big.png',
+				shopList: [{
+						id: 0,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 1,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 2,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					},
+					{
+						id: 3,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 4,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 5,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					}
+
+				]
+			},
+			recommendList: [{
+					id: 1,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 4,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				}
+			]
+		}
+	})
 })
 router.get('/api/index_list/jewelry', function(req, res, next) {
 	res.send({
@@ -321,6 +947,163 @@ router.get('/api/index_list/jewelry', function(req, res, next) {
 	})
 })
 router.get('/api/index_list/joke', function(req, res, next) {
-	res.send({})
+	res.send({
+		"code": 0,
+		"data": {
+			bannerImgUrl: '/static/index/banner/首饰珠宝.png',
+			iconList: [{
+					id: 0,
+					title: '黄金',
+					imgUrl: '/static/index/icon/首饰珠宝/1.png'
+				},
+				{
+					id: 1,
+					title: '吊坠',
+					imgUrl: '/static/index/icon/首饰珠宝/2.png'
+				},
+				{
+					id: 2,
+					title: '耳饰',
+					imgUrl: '/static/index/icon/首饰珠宝/3.png'
+				},
+				{
+					id: 3,
+					title: '手链',
+					imgUrl: '/static/index/icon/首饰珠宝/4.png'
+				},
+				{
+					id: 4,
+					title: '戒指',
+					imgUrl: '/static/index/icon/首饰珠宝/5.png'
+				},
+				{
+					id: 5,
+					title: '项链',
+					imgUrl: '/static/index/icon/首饰珠宝/6.png'
+				},
+				{
+					id: 6,
+					title: '手镯',
+					imgUrl: '/static/index/icon/首饰珠宝/7.png'
+				},
+				{
+					id: 7,
+					title: '转运珠',
+					imgUrl: '/static/index/icon/首饰珠宝/8.png'
+				}
+			],
+			hotList: [{
+					id: 1,
+					imgUrl: '/static/index/hot/首饰珠宝/hot1.png',
+					name: '周六福字母转运珠黄金定价足金情侣款diy手链手串手绳送新年礼物',
+					nprice: '611',
+					oprice: '655',
+					discount: '9.6'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/index/hot/首饰珠宝/hot2.png',
+					name: '周六福S925银珍珠耳饰女流光温柔法式优雅送女友新年礼物',
+					nprice: '198',
+					oprice: '218',
+					discount: '9.6'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/index/hot/首饰珠宝/hot3.png',
+					name: '周六福半山半水玉髓手镯浅白绿宽条新中式国风新年礼物送女友',
+					nprice: '409',
+					oprice: '800',
+					discount: '5'
+				}
+			],
+			shopData: {
+				shopImgUrl: '/static/index/shop/1/big.png',
+				shopList: [{
+						id: 0,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 1,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 2,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					},
+					{
+						id: 3,
+						imgUrl: '/static/index/shop/1/1.png',
+						name: '周生生三丽鸥家族珠光萌动足金黄金Hello Kitty珍珠手链女95958B',
+						nprice: '3490',
+						oprice: '7080',
+						discount: '4.9'
+					},
+					{
+						id: 4,
+						imgUrl: '/static/index/shop/1/2.png',
+						name: '周生生集团旗下品牌EMPHASIS融」系列18K金钻石项链94143N',
+						nprice: '9840',
+						oprice: '14000',
+						discount: '3.5'
+					},
+					{
+						id: 5,
+						imgUrl: '/static/index/shop/1/3.png',
+						name: '周生生18K白色黄金YuYu系列蓝宝石戒指95808R预订',
+						nprice: '25800',
+						oprice: '30066',
+						discount: '7.9'
+					}
+
+				]
+			},
+			recommendList: [{
+					id: 1,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 2,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 3,
+					imgUrl: '/static/commodity-item/2.png',
+					name: '【米哈游/崩坏：星穹铁道】砂金主题印象系列戒指套组 miHoYo',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				},
+				{
+					id: 4,
+					imgUrl: '/static/commodity-item/1.png',
+					name: '鸣潮薪火系列守岸人1/7比例手办 库洛游戏周边',
+					nprice: '349',
+					oprice: '708',
+					discount: '4.9'
+				}
+			]
+		}
+	})
 })
 module.exports = router;
